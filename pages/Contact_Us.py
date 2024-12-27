1cee72939e79097dadf4b0d58072695b3c1c7467
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-27 23:03:02 +0530

    day 23: updated contact us pagd UI

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_form", clear_on_submit=True):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print(button)
+        print("I am pressed")
+
