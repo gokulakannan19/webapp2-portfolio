71303a8f48d12c4c2d5e7d5aa0adc00595340a34
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-28 00:01:11 +0530

    day 23: send email function added and connected with forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,19 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact Me")
 
 with st.form(key="email_form", clear_on_submit=True):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Message")
+    raw_message = st.text_area("Message")
+    message = f"""\
+Subject: Portfolio Email
+
+From: {user_email}
+
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        print(button)
-        print("I am pressed")
-
+        send_email(message)
