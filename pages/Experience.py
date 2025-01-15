f5426ace6833d834fe864b7bc3539aab0917f023
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2025-01-15 23:06:04 +0530

    Experience page added

---
+++ pages/Experience.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+# Experience Section
+st.title("💼 Experience")
+
+# Job 1
+with st.expander("🚀 Consultant (Python Developer) | Optimum Solutions | July 2024 - Present"):
+    st.markdown("""
+    - Worked in <b>TCS - Standard Chartered Bank Project</b>
+    - Developed and maintained scalable web applications using <b>Python and Flask</b>.
+    - Worked with CI/CD pipeline automation, code versioning, and project management. 
+    - Improved system performance by 30% by optimizing backend logic.
+    """, unsafe_allow_html=True)
+
+# Job 2
+with st.expander("👨‍💻 Software Engineer | IDP Eductation Pvt Ltd | November 2022 - July 2024"):
+    st.markdown("""
+    - Designed and implemented a **scalable and flexible microservices architecture** using **Python, AWS Serverless, and AWS Lambda**.
+    - Upgraded Python version from **3.9 to 3.11**, leveraging new features and improving performance.
+    - Utilized **Linux and Bash scripting** for automation tasks, including deployment scripts and infrastructure setup, enhancing development efficiency.
+    - Implemented **CI/CD pipeline using GitLab CI/CD**, automating builds, testing, and deployment of microservices.
+    - Designed and documented **RESTful APIs** using **Swagger/OpenAPI specifications**, ensuring seamless integration between microservices.
+    - Collaborated with a development team, utilizing **Git and GitLab** for version control, code review, and efficient teamwork.
+    """, unsafe_allow_html=True)
+
+# Job 3
+with st.expander("👨‍💻 Full Stack Developer | Kyvor Genomics | November 2021 - October 2022"):
+    st.markdown("""
+    - **Analyzed product requirements** and contributed to designing/modifying application workflows and modules.
+    - Developed **scalable and efficient Python applications**, ensuring high performance and maintainability.
+    - **Tested and debugged** applications to ensure functionality, reliability, and optimal performance.
+    - Designed and implemented **RESTful APIs**, ensuring seamless communication between front-end and back-end services.
+    - Built **web applications using Django**, incorporating full **CRUD functionality, authentication, and authorization**.
+    - Automated **Google Sheets operations** using Python for efficient data management.
+    - Maintained and optimized **existing codebases**, improving system stability and performance.
+    """, unsafe_allow_html=True)
+
+# Job 4
+with st.expander("👨‍💻 Junior Analyst | IVTL Infoview Technologies | August 2019 - November 2021"):
+    st.markdown("""
+     - **Collected and analyzed data** to support business cases, ensuring informed decision-making in product and software development.
+    - Developed and executed **test plans and scripts** to track defects, ensuring high-quality software application development.
+    - Provided **technical and application support** to cross-functional teams, resolving issues efficiently.
+    - Designed and implemented **automation test scripts** and executed **manual test cases** to enhance software reliability.
+    - **Reviewed and analyzed product requirements**, ensuring clarity, consistency, and alignment with existing features.
+    """, unsafe_allow_html=True)
